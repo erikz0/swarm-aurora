fb70db11562bf9989c403beb596b8a8ab340967f
Erik Dale <erik.dale10@gmail.com>
2025-01-19 11:52:10 -0500

    added ability to set agent temp

--- swarm/core.py
+++ swarm/core.py
@@ -61,6 +61,7 @@ class Swarm:
             "tools": tools or None,
             "tool_choice": agent.tool_choice,
             "stream": stream,
+            "temperature": agent.temperature
         }
 
         if tools:
